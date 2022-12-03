5d70ccb705f6ffcfff8381909b4829af48fe608f
Thanu Poptiphueng <tpcrasite@gmail.com>
2022-12-04 00:31:40 +0700

    implemented 2021/day 20

--- src/bin/day20/main.rs
+++ src/bin/day20/main.rs
@@ -7,6 +7,20 @@ struct Map {
     data: Vec<bool>,
     is_background_fill: bool,
 }
+impl Map {
+    fn new(width: u32, height: u32) -> Self {
+        Self {
+            width,
+            height,
+            data: vec![false; (width * height) as usize],
+            is_background_fill: false,
+        }
+    }
+
+    fn get_total_light(&self) -> u32 {
+        self.data.iter().filter(|&&x| x).count() as u32
+    }
+}
 
 impl Grid for Map {
     type Item = bool;
@@ -22,6 +36,13 @@ impl Grid for Map {
     fn get_at_index(&self, index: usize) -> Option<Self::Item> {
         self.data.get(index).copied()
     }
+
+    fn display_unit(&self, item: Self::Item) -> char {
+        match item {
+            true => '#',
+            false => '.',
+        }
+    }
 }
 
 fn main() {
@@ -29,12 +50,82 @@ fn main() {
     let command = parse_command(input.next().unwrap());
     assert!(command.len() == 512);
     input.next().unwrap();
-    let map = parse_map(input);
-    dbg!(map
-        .get_from_coordinate(-1, -1)
-        .unwrap_or(map.is_background_fill));
-    dbg!(map.get_from_coordinate(1, 0));
-    dbg!(map.get_from_coordinate(2, 1));
+    let mut map = parse_map(input);
+    for _ in 0..50 {
+        let enchanced_map = enchance_map(&map, &command);
+        map = enchanced_map;
+    }
+    dbg!(map.get_total_light());
+}
+
+fn enchance_map(map: &Map, command: &[bool]) -> Map {
+    let mut enhanced_map = Map::new(map.width + 2, map.height + 2);
+    for (idx, current_point) in enhanced_map.data.iter_mut().enumerate() {
+        let center_point = Map::index_to_coord_from_width(idx, map.width as usize + 2);
+        let center_point = (center_point.0 as isize - 1, center_point.1 as isize - 1);
+        let mut total = 0;
+        if map
+            .get_from_coordinate(center_point.0 - 1, center_point.1 - 1)
+            .unwrap_or(map.is_background_fill)
+        {
+            total += 1 << 8;
+        }
+        if map
+            .get_from_coordinate(center_point.0, center_point.1 - 1)
+            .unwrap_or(map.is_background_fill)
+        {
+            total += 1 << 7;
+        }
+        if map
+            .get_from_coordinate(center_point.0 + 1, center_point.1 - 1)
+            .unwrap_or(map.is_background_fill)
+        {
+            total += 1 << 6;
+        }
+        if map
+            .get_from_coordinate(center_point.0 - 1, center_point.1)
+            .unwrap_or(map.is_background_fill)
+        {
+            total += 1 << 5;
+        }
+        if map
+            .get_from_coordinate(center_point.0, center_point.1)
+            .unwrap_or(map.is_background_fill)
+        {
+            total += 1 << 4;
+        }
+        if map
+            .get_from_coordinate(center_point.0 + 1, center_point.1)
+            .unwrap_or(map.is_background_fill)
+        {
+            total += 1 << 3;
+        }
+        if map
+            .get_from_coordinate(center_point.0 - 1, center_point.1 + 1)
+            .unwrap_or(map.is_background_fill)
+        {
+            total += 1 << 2;
+        }
+        if map
+            .get_from_coordinate(center_point.0, center_point.1 + 1)
+            .unwrap_or(map.is_background_fill)
+        {
+            total += 1 << 1;
+        }
+        if map
+            .get_from_coordinate(center_point.0 + 1, center_point.1 + 1)
+            .unwrap_or(map.is_background_fill)
+        {
+            total += 1;
+        }
+        *current_point = command[total];
+    }
+    if map.is_background_fill {
+        enhanced_map.is_background_fill = *command.last().unwrap();
+    } else {
+        enhanced_map.is_background_fill = *command.first().unwrap();
+    }
+    enhanced_map
 }
 
 fn parse_command(input: &str) -> Vec<bool> {
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,23 @@
+use std::fmt::Display;
+
 pub trait Grid {
-    type Item;
+    type Item: Display;
     fn width(&self) -> usize;
     fn height(&self) -> usize;
     fn get_at_index(&self, index: usize) -> Option<Self::Item>;
+    fn index_to_coord_from_width(index: usize, width: usize) -> (usize, usize) {
+        let x = index % width;
+        let y = index / width;
+        (x, y)
+    }
+    fn index_to_coord(&self, index: usize) -> (usize, usize) {
+        let width = self.width();
+        let x = index % width;
+        let y = index / width;
+        (x, y)
+    }
     fn get_from_coordinate(&self, x: isize, y: isize) -> Option<Self::Item> {
-        if x < 0 || y < 0 {
+        if x < 0 || y < 0 || x >= self.width() as isize || y >= self.height() as isize {
             None
         } else {
             self.get_at_index(y as usize * self.width() + x as usize)
@@ -12,5 +25,20 @@ pub trait Grid {
     }
     fn get_from_index(&self, index: usize) -> Option<Self::Item> {
         self.get_at_index(index)
+    }
+
+    fn display_unit(&self, item: Self::Item) -> char;
+
+    fn draw(&self) {
+        for y in 0..self.height() {
+            for x in 0..self.width() {
+                print!(
+                    "{}",
+                    self.display_unit(self.get_from_coordinate(x as isize, y as isize).unwrap())
+                );
+            }
+            println!();
+        }
+        println!();
     }
 }
